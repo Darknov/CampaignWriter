a5cd6a3db14b783027f63941bda963460ca5ee81
Darknov <darknov.dono@gmail.com>
2018-06-05 18:12:58 +0200

    Revamp into new app
    I will use Riot.js framework to make the app cuz it is lightweight.
    Remove JQuery - not really used so much those times.
    Add animate.css for fancy animations.
    Remove bootstrap js because I will only need css. I will make sure
    to check other options.
    
    Overall development should be much easier, quicker and code should
    be much clearer. Will see :)

--- js/app.js
+++ js/app.js
@@ -2,24 +2,26 @@
  *  App
  */
 
-const app = (function () {
-  let categories = [];
-
-  return {
-    getCategoryLength: () => categories.length,
-    addCategory: () => {
-      let c = new Category(categories.length, "New Category")
-      categories.push(c);
-      return c;
-    },
-    getCategory: (id) => categories[id],
-  };
-})();
-
-
-
+const app = {
+  categories: []
+}
 
 export const leftMenu = document.getElementById('leftMenu'),
   story = document.getElementById('story'),
   storyPanel = document.getElementById('storyPanel'),
   storyButton = document.getElementById('storyButton');
+
+
+export function getCategoryLength() {
+  return app.length;
+}
+
+export function addCategory(category) {
+  app.categories.push(category);
+  return category;
+}
+
+export function getCategory() {
+
+}
+
